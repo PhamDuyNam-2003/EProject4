a4ad0215b0a38f018d1585792f31976853555e2a
Pham Duy Nam <trollmlbbtop@gmail.com>
2026-07-11 13:10:30 +0700

    chore: sync backend folder structure with google sheet

--- project-root/backend/services/inventory-service/Inventory-service/Inventory-service/Application/Interfaces/IRedisLockService.cs
+++
@@ -1,8 +0,0 @@
-namespace Inventory_service.Application.Interfaces
-{
-    public interface IRedisLockService
-    {
-        Task<bool> AcquireLockAsync(string lockKey, string lockValue, TimeSpan expiry);
-        Task ReleaseLockAsync(string lockKey, string lockValue);
-    }
-}
