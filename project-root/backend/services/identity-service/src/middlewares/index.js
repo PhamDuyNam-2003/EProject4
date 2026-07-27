14eae6eec9cca36f9eb56b8b8a1972f25694c9e1
Pham Duy Nam <trollmlbbtop@gmail.com>
2026-07-27 21:54:02 +0700

    Fix: Remove generated .js files from src and add outDir to tsconfig

--- project-root/backend/services/identity-service/src/middlewares/index.js
+++
@@ -1,5 +0,0 @@
-export * from "@/middlewares/authMiddleware";
-export * from "@/middlewares/errorMiddleware";
-export * from "@/middlewares/roleMiddleware";
-export * from "@/middlewares/rateLimitMiddleware";
-export * from "@/middlewares/validationMiddleware";
